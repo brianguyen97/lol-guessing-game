d99e2f31a64237cf366da36d7896f629c07c2c33
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-05-22 03:23:10 -0600

    Initial commit

--- database.js
+++ database.js
@@ -1,9 +1,11 @@
 const hints = {
   Aatrox:
-    'This champion is an an attack damage character and their primary role is top.',
-  Ahri: 'This champion is an an ability power character and their primary role is mid.',
+    "This champion is an an attack damage character and their primary role is top.",
+  Ahri: "This champion is an an ability power character and their primary role is mid.",
   Akali:
-    'This champion is an ability power character, and their primary roles are top and mid.',
+    "This champion is an ability power character, and their primary roles are top and mid.",
   Akshan:
-    'This champion is an attack damage character, and their primary roles are top and mid.',
+    "This champion is an attack damage character, and their primary roles are top and mid.",
+  AurelionSol:
+    "This champion is an abilty power champion and their primary role is mid",
 };
