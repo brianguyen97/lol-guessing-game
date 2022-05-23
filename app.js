67d7586a1722d945ba28c8bd000de32dda21bbc7
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-05-22 20:37:27 -0600

    Fix bug?

--- app.js
+++ app.js
@@ -44,12 +44,10 @@ $(document).ready(() => {
   const champ = random(championsArray);
   const spell = random(qwer);
 
+  $('.container').append(`<img src="done/${champ}${spell}.png" alt=""></img>`);
   // $('.container').append(
-  //   `<img src="../done/${champ}${spell}.png" alt=""></img>`
+  //   `<img src="https://github.com/brianguyen97/lol-guessing-game/blob/main/done/${champ}${spell}.png" alt="champion-ability-icon"> </img>`
   // );
-  $('.container').append(
-    `<img src="https://github.com/brianguyen97/lol-guessing-game/blob/main/done/${champ}${spell}.png" alt="champion-ability-icon"> </img>`
-  );
 
   $('.form').submit(e => {
     e.preventDefault();
