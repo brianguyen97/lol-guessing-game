44eab8b7c813a3b4277fd475e459473c25df15bf
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-05-22 19:55:54 -0600

    Add more champions to hints

--- database.js
+++ database.js
@@ -1,11 +1,50 @@
 const hints = {
   Aatrox:
-    "This champion is an an attack damage character and their primary role is top.",
-  Ahri: "This champion is an an ability power character and their primary role is mid.",
+    'This champion is an an attack damage character and their primary role is top.',
+  Ahri: 'This champion is an an ability power character and their primary role is mid.',
   Akali:
-    "This champion is an ability power character, and their primary roles are top and mid.",
+    'This champion is an ability power character and their primary roles are top and mid.',
   Akshan:
-    "This champion is an attack damage character, and their primary roles are top and mid.",
+    'This champion is an attack damage character and their primary roles are top and mid.',
   AurelionSol:
-    "This champion is an abilty power champion and their primary role is mid",
+    'This champion is an abilty power character and their primary role is mid.',
+  Azir: 'This champion is an ability power character and their primary role is mid.',
+  Brand:
+    'This champion is an ability power character and their primary role is support.',
+  Evelynn:
+    'This champion is an ability power character and their primary role is jungle.',
+  Yuumi:
+    'This champion is an ability power character and their primary role is support.',
+  Yone: 'This champion is an attack damage character and their primary roles are top and mid.',
+  Zyra: 'This champion is an ability power character and their primary role is support.',
+  Zoe: 'This champion is an ability power character and their primary role is mid.',
+  Ornn: 'This champion is a tank and their primary role is top.',
+  Skarner: 'This champion is AD/Tank and their primary role is jungle.',
+  Swain:
+    "This champion is an ability power character and they're played everywhere but jungle.",
+  Nunu: 'This champion is a tank and their primary role is jungle.',
+  Lulu: 'This champion is an ability power character and their main role is support.',
+  Lissandra:
+    'This champion is an ability power character and their main role is mid.',
+  Lillia:
+    'This champion is an ability power character and their main role is jungle.',
+  LeBlanc:
+    'This champion is an ability power character and their main role is mid.',
+  Kled: 'This champion is an an attack damage character and their main role is top.',
+  Kindred:
+    'This champion is an attack damage character and their main role is jungle.',
+  Ivern:
+    'This champion is an ability power character and their main role is jungle.',
+  Hecarim:
+    'This champion is an attack damage character and their main role is jungle.',
+  Ekko: 'This champion is an ability power character and their main role is mid.',
+  Ezreal:
+    'This champion is an attack damage character and their main role is ADC.',
+  Fiddlesticks:
+    'This champion is an ability power character and their main role is jungle.',
+  Fizz: 'This champion is an ability power character and their main role is mid.',
+  Garen:
+    'This champion is an attack damage character and their main role is top.',
+  Gangplank:
+    'This champion is an attack damage character and their main role is top.',
 };
